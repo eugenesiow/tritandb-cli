2d5f1575b81c4431509747e0175ff31a24600e80
Eugene Siow <kyo116@gmail.com>
2017-09-21 18:20:51 +0100

    Add query scripts

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-java -jar tritandb-cli --query --file="queries/shelburne/cols.sparql"
+java -jar tritandb-cli.jar --query --file="queries/shelburne/cols.sparql"
+java -jar tritandb-cli.jar --query --file="queries/shelburne/row.sparql"
+java -jar tritandb-cli.jar --query --file="queries/shelburne/aggr.sparql"
